7994336482439a0a4d68ef7411555463f994c774
Edmar Alves <47756547+EdmarAlvez@users.noreply.github.com>
2020-04-02 16:51:55 -0300

    AprendendoJavaLoops
    Loops

---
+++ LoopsPrincipal.java
@@ -0,0 +1,26 @@
+package com.projeto.loops;
+
+public class LoopsPrincipal {
+	//while
+	
+	public static void main(String[] args) {
+		
+		int numero = 0;
+		
+		
+		/*while(numero < 5) {
+			//corpo
+			System.out.println("Loop" + numero);
+			numero++;
+		}*/
+		
+		/*do {
+			System.out.println("Loop" + numero);
+			numero++;
+		}while(numero <5);*/
+		for(int n=1; n<=5; n++) {
+			System.out.println("Loop" + n);
+		}
+	}
+
+}
