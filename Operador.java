34caf77b7562dcf405e502ca13ad74e345683bfc
Edmar Alves <47756547+EdmarAlvez@users.noreply.github.com>
2020-04-02 15:48:47 -0300

    AprendendoJava
    Aprendendo JavaBasico.

---
+++ Operador.java
@@ -0,0 +1,21 @@
+package com.projeto.operadoresbasicos;
+
+public class Operador {
+	
+	//somar(+)
+	//subtrair(-)
+	//multiplicar(*)
+	//dividir(/)
+	//Modulo (Resto da divisão)(%)
+	public static void main(String[] args) {
+		
+		int primeiro = 7;
+		int segundo = 2;
+		int resultado = 0;
+		
+		resultado = primeiro % segundo;
+		
+		System.out.println(resultado);
+	}
+
+}
